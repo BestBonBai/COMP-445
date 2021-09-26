14a60b2ad724a39a2494e5f2e1090ec59d588b24
BestBonBai <jsbaihualin@hotmail.com>
2021-09-25 23:16:48 -0400

    restructure code

--- A1/httpc.py
+++ A1/httpc.py
@@ -13,6 +13,7 @@ import re
 import socket
 from urllib.parse import urlparse
 from HttpClient import HttpRequest, HttpResponse
+import shlex # ignore the space in quotes " x x"
 
 class Httpc(cmd.Cmd):
     """ 
@@ -109,7 +110,7 @@ class Httpc(cmd.Cmd):
         parser_get.add_argument('url',help='a valid http url',default=cmd.split()[-1],nargs='?' )
         # print(cmd.split()[-1])
         # print(cmd.split()[:-1])
-        print("[Debug] cmd.split() is : " + str(cmd.split()) )
+        # print("[Debug] cmd.split() is : " + str(cmd.split()) )
 
         args = parser_get.parse_args(cmd.split()[:-1])
         # print parser help
@@ -122,61 +123,14 @@ class Httpc(cmd.Cmd):
             # recall HttpClient to send Http request
         
             # urlparse 
-            url = eval(args.url)
-            url = urlparse(url)
-
-            scheme = url.scheme
-            hostname = url.hostname
-            # websocket has 2 types : ws and wss, the port is 80 and 443 respectively.
-            port = url.port or (443 if scheme == 'wss' else 80)
-            ip_address = socket.gethostbyname(hostname)
-            resource = url.path
-            if url.query:
-                resource += '?' + url.query
-
-            # print(f'[Debug] url : {url}')
-
-            # use socket to connect server
-            client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            address = (ip_address, port)
-
-            try:
-                client_socket.connect(address)
-                print('--- Connect success ---')
-                data = b''
-
-                if args.header and self._is_valid_header(args.header):
-                    # case: one more key:value headers
-                    headers = ''
-                    for i in range(len(args.header)):
-                        headers += args.header[i] + '\r\n'
-                    request = HttpRequest(hostname,url.path, url.query, headers)
-                else:
-                    request = HttpRequest(hostname,url.path, url.query,)   
-                request = request.get_request_get()
-
-                while True:
-                    # send data
-                    client_socket.sendall(request.encode("utf-8"))
-                    # receive data
-                    # MSG_WAITALL waits for full request or error
-                    response = client_socket.recv(len(request), socket.MSG_WAITALL)
-                    data += response
-                    if len(response) < 1 : break
-
-                result = HttpResponse(data)
-
-            finally:      
-                client_socket.close()
+            url_parsed = URL_PARSE(args.url)
+            # get request  
+            request = self._get_request(url_parsed,args,'GET')
+            # use socket to connect server, get response
+            response_content = self._client_socket_connect_server(url_parsed,request)
 
             # Output depends on diffenrent requirements (-v)
-            if args.verbose:
-                print('--- Details ---')
-                print(result.content)
-            # other cases
-            else: 
-                # only print the content.body
-                print(result.body)
+            self._print_details_by_verbose(args.verbose,response_content)
 
     
     # some private methods
@@ -202,22 +156,109 @@ class Httpc(cmd.Cmd):
         '''
         # case considers one more key:value
         if len(header) >= 1:
+            # check whether header is valid one by one
             for i in range(len(header)):
-                if re.match(r'(\w+:\w+)',header[i]):
+                if re.match(r'(.+:.+)',header[i]):
                     print('[Debug] valid header : ' + header[i])
-                    return True
                 else: 
                     print('[Debug] invalid header : ' + header[i])
                     return False
         else:
             print('[Debug] no header ')
             return False
+        return True
 
+    def _get_request(self, url_parsed, args, request_method):
+        '''
+        The method is to get request by parsed URL, headers, and request method.
+        :param: url_parsed
+        :param: args : need args.header, args.data, args.file
+        :param: request_method : GET or POST
+        :return: request
+        '''
+        if args.header and self._is_valid_header(args.header):
+            # case: one more key:value headers
+            headers = ''
+            for i in range(len(args.header)):
+                headers += args.header[i] + '\r\n'
+            if request_method == 'GET':
+                request = HttpRequest(url_parsed.hostname, url_parsed.path, url_parsed.query, headers)
+            else:
+                # POST query means infos of (-d) data or (-f) file
+                if args.data:
+                    request = HttpRequest(url_parsed.hostname, url_parsed.path, args.data, headers)
+                else:
+                    # TODO: get the info of files
+                    pass
+        else:
+            if request_method == 'GET':
+                request = HttpRequest(url_parsed.hostname, url_parsed.path, url_parsed.query,)  
+            else: 
+                # POST query means infos of (-d) data or (-f) file
+                if args.data:
+                    request = HttpRequest(url_parsed.hostname, url_parsed.path, args.data,)
+                else:
+                    # TODO: get the info of files
+                    pass
+        # get request
+        request = request.get_request(request_method)
+
+        return request
+
+    def _client_socket_connect_server(self, url_parsed, request):
+        '''
+        The method is to connect server using client socket.
+        :param: url_parsed
+        :param: request
+        :param: args
+        :return: response_content
+        '''
+        # use socket to connect server, socket.SOCK_STREAM is for TCP
+        client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        try:
+            client_socket.connect(url_parsed.address)
+            print('--- Connect success ---')
+            
+            # get response : data (Type: bytes) that need to be decoded by UTF-8 
+            data = b''
+            while True:
+                # send data
+                client_socket.sendall(request.encode("utf-8"))
+                # receive data
+                # MSG_WAITALL waits for full request or error
+                response = client_socket.recv(len(request), socket.MSG_WAITALL)
+                if response:
+                    data += response
+                else: break
+                # if len(response) < 1 : break
+            # decode data, and then parse content
+            response_content = HttpResponse(data)
+
+        finally:      
+            client_socket.close()
+            return response_content
+        
+    def _print_details_by_verbose(self,args_verbose, response_content):
+        '''
+        The method is to show different output depends on verbose.
+        :param: args_verbose
+        :param: response_content
+        :return: None
+        '''
+        # Output depends on diffenrent requirements (-v)
+        if args_verbose:
+            print('--- Details ---')
+            print(response_content.content)
+        # other cases
+        else: 
+            # only print the content.body
+            print(response_content.body)
 
     def do_post(self,cmd):
         '''
         The method is to executes a HTTP POST request for a given URL with inline data or from file.
         :param: cmd : command from console
+        :test: httpc post -h Content-Type:application/json -d '{"Assignment": 1}' http://httpbin.org/post
         '''
         # parse the command from console
         parser_post = argparse.ArgumentParser(description='Post executes a HTTP POST request for a given URL with inline data or from file.'
@@ -226,16 +267,66 @@ class Httpc(cmd.Cmd):
         parser_post.usage = parser_post.prog + ' [-v] [-h key:value] [-d inline-data] [-f file] URL'
         parser_post.epilog = 'Either [-d] or [-f] can be used but not both. '
         # add_mutually_exclusive_group
-        parser_post.add_mutually_exclusive_group(required = True)
-        parser_post.add_argument('-v',help='Prints the detail of the response such as protocol, status, and headers.')
-        parser_post.add_argument('-h',help='Associates headers to HTTP request with the format \'key:value\' ')
-        parser_post.add_argument('-d',help='Associates an inline data to the body HTTP POST request.', action='store_true')
-        parser_post.add_argument('-f',help='Associates the content of a file to the body HTTP POST request.', action='store_false')
-        # add position argument URL
-        parser_post.add_argument('url',help='a valid http url')
-        args = parser_post.parse_args(cmd.split())
+        
+        parser_post.add_argument('-v','--verbose',help='Prints the detail of the response such as protocol, status, and headers.', action='store_true' )
+        parser_post.add_argument('-h', '--header', help='Associates headers to HTTP request with the format \'key:value\' ', nargs='+')
+        parser_post.add_mutually_exclusive_group()
+        parser_post.add_argument('-d','--data',help='Associates an inline data to the body HTTP POST request.')
+        parser_post.add_argument('-f','--file',help='Associates the content of a file to the body HTTP POST request.')
+        # add position argument URL, default is to make sure the last argument is URL, add \' for eval() function
+        parser_post.add_argument('url',help='a valid http url',default= "'" + shlex.split(cmd)[-1] + "'" ,nargs='?' )
+        
+        print('[Debug] args is : ' + str(shlex.split(cmd) ))
+        args = parser_post.parse_args(shlex.split(cmd)[:-1])
+
+        print("[Debug] args are : " + str(args) )
         # test
         # parser_post.print_help()
+        
+        # Check the URL is valid
+        if self._is_valid_url(args.url):
+            # recall HttpClient to send Http request
+        
+            # urlparse 
+            url_parsed = URL_PARSE(args.url)
+            # get request  
+            request = self._get_request(url_parsed,args,'POST')
+            # use socket to connect server, get response
+            response_content = self._client_socket_connect_server(url_parsed,request)
+
+            # Output depends on diffenrent requirements (-v)
+            self._print_details_by_verbose(args.verbose,response_content)
+        
+
+class URL_PARSE:
+    '''
+    The class is to parse the url and store the parameters of url_parsed.
+    :params: url, scheme, hostname, port, ip_address, resource, address
+    '''
+    def __init__(self, args_url):
+        # urlparse 
+        self.url = eval(args_url)
+        self.url = urlparse(self.url)
+        self._assign_param_url()
+
+    def _assign_param_url(self):
+        '''
+        The method is to assign parameters of URL.
+        '''
+        # assign parameters of url
+        self.scheme = self.url.scheme
+        self.hostname = self.url.hostname
+        self.path = self.url.path
+        self.query = self.url.query
+        # websocket has 2 types : ws and wss, the port is 80 and 443 respectively.
+        self.port = self.url.port or (443 if self.scheme == 'wss' else 80)
+        self.ip_address = socket.gethostbyname(self.hostname)
+        self.resource = self.path
+        if self.url.query:
+            self.resource += '?' + self.query
+        # for socket address
+        self.address = (self.ip_address, self.port)
+
 
 # main
 if __name__ == '__main__':
